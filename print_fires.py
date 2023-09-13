adce4c83388c67f35123ba51653bc1f93527c8a6
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-09-12 21:05:50 -0600

    Modify to accept args from run.sh

--- print_fires.py
+++ print_fires.py
@@ -1,9 +1,15 @@
 import my_utils as utils
+import sys
 
-country='United States of America'
-country_column = 0
-fires_column = 3
-file_name = 'Agrofood_co2_emission.csv'
-fires = utils.get_column(file_name, country_column, country,
+file_name = sys.argv[1]
+country_column = int(sys.argv[2])
+country = sys.argv[3]
+
+if len(sys.argv) == 5:
+    fires_column = int(sys.argv[4])
+    fires = utils.get_column(file_name, country_column, country,
                          result_column = fires_column)
+else:
+    fires = utils.get_column(file_name, country_column, country)
+
 print(fires)
