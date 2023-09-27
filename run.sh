fb8c77c6f3e1a6529e43980ac8d8f003f643f3f1
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-09-26 19:18:15 -0600

    Update args, add examples

--- run.sh
+++ run.sh
@@ -4,28 +4,37 @@ set -e  # stop on error
 set -u  # raise error if variable is unset
 set -o pipefail  # fail if any prior step failed
 
-if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]
-then
-    echo "Usage: run.sh <file_name> <query_column> <query_value>"
-    echo "[optional result_column]"
-    echo "Script runs print_fires.py given file name, column to query"
-    echo "(int), value to query, and optional result column (int)."
-    exit
-fi
+# Script runs print_fires.py given file name, column to query
+# (int), value to query, and optional result column (int).
 
-file_name=$1
-query_column=$2
-query_value=$3
+# Set parameters
+file_name="data/Agrofood_co2_emission.csv"
+file_name_bad="data/Agrofoooood_co2_emission.csv"
+query_column=0
+query_value="United States of America"
+result_column=4
+result_column_bad=1000
 
-echo $3
-echo $query_value
+# Working example of running print_fires.py with result column
+echo "Running print_fires.py with result column $result_column"
+python3 print_fires.py \
+    --file-name "$file_name" --country-column $query_column \
+    --country "$query_value" --fires-column $result_column
 
-if [ "$#" -eq 4 ]
-then
-    result_column=$4
-    echo "Running print_fires.py with result column $result_column"
-    python3 print_fires.py "$file_name" "$query_column" "$query_value" "$result_column"
-else
-    echo "Running print_fires.py with default result column"
-    python3 print_fires.py $file_name $query_column "$query_value"
-fi
+# Working example of running print_fires.py with default result column
+echo "Running print_fires.py with default result column"
+python3 print_fires.py \
+    --file-name "$file_name" --country-column $query_column \
+    --country "$query_value"
+
+# Broken example of running print_fires.py, with result column out
+# of file index
+set +e  # unset exit on error
+python3 print_fires.py \
+    --file-name "$file_name" --country-column $query_column \
+    --country "$query_value" --fires-column $result_column_bad
+
+# Broken example of running print_fires.py, with bad file name
+python3 print_fires.py \
+    --file-name "$file_name_bad" --country-column $query_column \
+    --country "$query_value"
