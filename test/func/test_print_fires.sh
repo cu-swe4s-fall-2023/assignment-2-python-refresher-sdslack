465f53b01f05bb54e74d4723b38cbcc23a49e352
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-10-04 22:54:27 -0600

    First pass add func tests, fix errors

---
+++ test/func/test_print_fires.sh
@@ -0,0 +1,42 @@
+test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest/master/ssshtest
+. ssshtest
+
+run print_fires_help python src/print_fires.py -h
+assert_exit_code 0
+assert_in_stdout "usage: print_fires"
+
+run print_fires_default_res python src/print_fires.py \
+    --file-name "test/data/test.csv" \
+    --country-column 0 \
+    --country "Argentina"
+assert_exit_code 0
+assert_stdout
+
+run print_fires_choose_res python src/print_fires.py \
+    --file-name "test/data/test.csv" \
+    --country-column 0 \
+    --country "United States of America" \
+    --fires-column 10
+assert_exit_code 0
+assert_stdout
+
+run print_fires_bad_file python src/print_fires.py \
+    --file-name "test/data/no_test_file.csv" \
+    --country-column 0 \
+    --country "Argentina"
+assert_exit_code 1
+
+run print_fires_res_non_numeric python src/print_fires.py \
+    --file-name "test/data/test.csv" \
+    --country-column 0 \
+    --country "United States of America" \
+    --fires-column 0
+assert_exit_code 1
+assert_stdout "Could not convert result to float, so can't convert to int."
+
+run print_fires_query_col_neg python src/print_fires.py \
+    --file-name "test/data/test.csv" \
+    --country-column -1 \
+    --country "United States of America"
+assert_exit_code 1
+assert_stdout "Query column must be positive."
