957b902c48b1efbb11f09060ebbdf77382841b35
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2023-11-09 07:15:33 -0700

    Add test data that preserves data structure

--- UsingLibraries/test/unit/test_lib_utils.py
+++ UsingLibraries/test/unit/test_lib_utils.py
@@ -7,28 +7,11 @@ import lib_utils
 
 
 class TestLibUtils(unittest.TestCase):
-    def setUp(self):
-        # Set up test agro file
-        self.test_agro_file = 'test_agro_file.csv'
-        f = open(self.test_agro_file, 'w')
-        f.write('Angola,1990,14.5,0.05,205\n')
-        f.write('Brazil,2001,0.0,0.0,926.3\n')
-        f.write('Brazil,2006,14500,115.2,20006.2\n')
-        f.write('Brazil,2012,1333,58.3,1.6\n')
-        f.close()
-        # Set up test GDP file
-        self.test_gdp_file = 'test_gdp_file.csv'
-        f = open(self.test_gdp_file, 'w')
-        f.write('Country,1950,1990,2001,2012\n')
-        f.write('Angola,...,...,...,...\n')
-        f.write('Brazil,...,17,000.35,180,760.00,900,000.99\n')
-        f.close()
-
-    def tearDown(self):
-        os.remove(self.test_agro_file)
-        os.remove(self.test_gdp_file)
 
     def test_get_data(self):
-        agro_df, gdp_df = lib_utils.get_data(test_agro_file.csv, test_gdp_file.csv)
-        self.assertEqual(agro_df.shape, (4, 5))
-        self.assertEqual(gdp_df.shape, (4, 5))
+        # Using small test files instead of making new to preserve
+        # the formatting in target files
+        agro_df, gdp_df = lib_utils.get_data('../data/test_Agrofood_co2_emission.csv',
+                                             '../data/test_IMF_GDP.csv')
+        self.assertEqual(agro_df.shape, (26, 31))
+        self.assertEqual(gdp_df.shape, (3, 40))
